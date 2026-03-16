877f5f7110ae0435149083c4ef2451c7282c76ab
Gideon Dijkhuis <dijkhuis.g.a@outlook.com>
2026-03-16 19:08:48 +0100

    feat: First iteration of Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,25 @@
+import {Button, Subtitle1, Title2} from "@fluentui/react-components";
+
 const Footer = () => {
     return (
         <>
+            <div className={`flex flex-wrap-reverse`}>
+                <div className={`flex-1`}>
+                    <ul>
+
+                    </ul>
+                    <Subtitle1 className={`text-no-margin`}>Email</Subtitle1>
+                    <Button as={`a`} href={`mailto:bernhold@belius.nl`} target="_blank" appearance={`subtle`} rel="noopener noreferrer">bernhold@belius.nl</Button>
+                </div>
+                <div className={`flex-1`}>
+                    <b className={`text-no-margin`} >Snel naar: </b>
+                    <ul>
+                        <li>Start</li>
+                        <li>News</li>
+                        <li>Over</li>
+                    </ul>
+                </div>
+            </div>
         </>
     );
 }
